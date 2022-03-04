c8b59a168d68326e669474b25965312ad27a1074
nebcio <73241633+nebcio@users.noreply.github.com>
2022-03-04 12:05:20 +0100

    Lista1

---
+++ lista1.kt
@@ -0,0 +1,52 @@
+fun missingNumber(tab: IntArray) : Int{
+    tab.sort()
+    for (i in 0 until tab.size) {
+        if (i != tab[i]) {
+            return i
+        }
+    }
+    return tab.size
+}
+
+fun main() {
+
+    // 1, 2, 3
+    for (i in 1..100) {
+        var podzielnosc: String = ""
+        if (i%3 == 0) {
+            podzielnosc += "trzy"
+        }
+        if (i%5 == 0) {
+            podzielnosc += "piec"
+        }
+        if (i%7 == 0) {
+            podzielnosc += "siedem"
+        }
+        if (i%11 == 0) {
+            podzielnosc += "jedenascie"
+        }
+        if (i%13 == 0) {
+            podzielnosc += "trzynascie"
+        }
+        if (podzielnosc == "") {
+            println(i)
+        }
+        else {
+            println(podzielnosc)
+        }
+    }
+
+    // 4
+    val ar: IntArray = intArrayOf(0, 1, 3)
+    println("\n\nW tablicy: ")
+    for (i in ar) print("$i ")
+    val mN = missingNumber(ar)
+    println("\nBrakuje: $mN")
+
+    val ar2: IntArray = intArrayOf(0, 1, 3, 2)
+    println("\n\nW tablicy: ")
+    for (i in ar2) print("$i ")
+    val mN2 = missingNumber(ar2)
+    println("\nBrakuje: $mN2")
+
+}
